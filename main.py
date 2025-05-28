1307622e9d90e7ac75b1a0f518a79a9ff13daef2
David Vega Tepetla <pescador2111@hotmail.com>
2025-05-27 23:21:15 -0600

    se ha agregado la funcionalidad de mostrar las tareas en consola, falta poder agregar

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random as ra
 import json
 from typing_extensions import Annotated
 from rich.console import Console
+from rich.table import Table
 import typer
 
 app = typer.Typer()
@@ -15,6 +16,20 @@ class list_item():
 
     def __str__(self):
         return "{" + f'"task":"{self.task}"' + "}"
+
+
+tasks = []
+
+
+def print_tasks():
+    table = Table("index", "tasks", "state")
+    # temp = '{"sadsa":"sadas", "asdasd":23132312, "asdasdsa":"qwewqeqw"}'
+    with open("todo.md", encoding="UTF-8") as f:
+        for x in f:
+            line = json.loads(x)
+            # console.print_json(data=line)
+            table.add_row(line["index"], line["tascks"], str(line["state"]))
+    console.print(table)
 
 
 @app.command()
@@ -34,20 +49,19 @@ def check():
 
 @app.command()
 def list(
-    task: Annotated[str, typer.Option(help="Tarea que se desea ingresar")],
-    name: Annotated[str | None, typer.Argument()] = None
+    # task: Annotated[str, typer.Option(help="Tarea que se desea ingresar")],
+    # name: Annotated[str | None, typer.Argument()] = None
 ):
-    print("Todo list")
-    index = ra.random()
-    print(index)
-    if not os.path.exists("todo.txt"):
-        with open("todo.txt", "x") as f:
-            f.write("no existe")
-    else:
-        with open("todo.txt") as f:
-            for x in f:
-                console.print("[red]"+x+"[/red]")
+    # index = ra.random()
+    print_tasks()
 
 
 if __name__ == '__main__':
+    if not os.path.exists("todo.md"):
+        with open("todo.md", "x") as f:
+            f.write("")
+    # else:
+    #     with open("todo.txt") as f:
+    #         for x in f:
+    #             console.print("[red]"+x+"[/red]")
     app()
